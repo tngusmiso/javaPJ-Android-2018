77f9ae293ca94ed79b6f6ed2629d44084ee88ab4
tngusmiso <k1miso012@gmail.com>
2018-10-01 20:40:55 +0900

    1001 set the Intent goToWorkingSpace from MainActivity to WorkingSpace

--- AndroidVideoEditor/app/src/main/java/cse/lsh/javapj/androidvideoeditor/MainActivity.java
+++ AndroidVideoEditor/app/src/main/java/cse/lsh/javapj/androidvideoeditor/MainActivity.java
@@ -1,7 +1,9 @@
 package cse.lsh.javapj.androidvideoeditor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void goToWorkingSpace(View v){
+        Intent intent = new Intent(MainActivity.this, WorkingSpace.class);
+        startActivity(intent);
+        finish();   //다음 액티비티로 넘어가면 현재 액티비티 소멸
     }
 }
