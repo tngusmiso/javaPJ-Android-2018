2a3fdbfdc0065b62c606a0cf104695d6e5658aba
tngusmiso <k1miso012@gmail.com>
2018-11-07 23:37:54 +0900

    [1107] add top_menu.xml - to inflate

--- DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
+++ DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
@@ -2,6 +2,8 @@ package cse.moblie.ducks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final LinearLayout menuCont = (LinearLayout)findViewById(R.id.topMenuContainer);
+        LayoutInflater inflater = getLayoutInflater();
+        inflater.inflate(R.layout.top_menu, menuCont);
     }
 }
