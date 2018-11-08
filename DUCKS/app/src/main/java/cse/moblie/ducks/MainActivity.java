9c8d03d2628b8962863007eb5c88a036e33a4897
tngusmiso <k1miso012@gmail.com>
2018-11-08 23:44:34 +0900

    [1108] Change the theme colors and set the App Bar

--- DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
+++ DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
@@ -3,6 +3,7 @@ package cse.moblie.ducks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final LinearLayout menuCont = (LinearLayout)findViewById(R.id.topMenuContainer);
-        LayoutInflater inflater = getLayoutInflater();
-        inflater.inflate(R.layout.top_menu, menuCont);
+//        final LinearLayout menuCont = (LinearLayout)findViewById(R.id.topMenuContainer);
+//        LayoutInflater inflater = getLayoutInflater();
+//        inflater.inflate(R.layout.top_menu, menuCont);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_actions, menu) ;
+        return true ;
+    }
+
+
 }
