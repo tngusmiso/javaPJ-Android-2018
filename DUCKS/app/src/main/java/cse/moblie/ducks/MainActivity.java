fdac0759fefabb48698d1f3df026d3a1c0c4dac3
tngusmiso <k1miso012@gmail.com>
2018-11-13 20:35:22 +0900

    Set Top Bar and Search

--- DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
+++ DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,16 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        final LinearLayout menuCont = (LinearLayout)findViewById(R.id.topMenuContainer);
-//        LayoutInflater inflater = getLayoutInflater();
-//        inflater.inflate(R.layout.top_menu, menuCont);
+        // 상단앱바 설정
+        RelativeLayout topBar = findViewById(R.id.rlTopBar);
+        LayoutInflater inflater = getLayoutInflater();
+        inflater.inflate(R.layout.top_bar, topBar);
+
+        // 상단메뉴 설정
+        RelativeLayout menuCont = findViewById(R.id.rlTopMenu);
+        inflater.inflate(R.layout.top_menu, menuCont);
+
+        // 메뉴버튼
+        Button btMenu = findViewById(R.id.btSide);
+        btMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout rlTopMenu = findViewById(R.id.rlTopMenu);
+                if (rlTopMenu.getVisibility() == View.GONE) {
+                    rlTopMenu.setVisibility(View.VISIBLE);
+                } else rlTopMenu.setVisibility(View.GONE);
+            };
+        });
+
+        // 검색버튼
+        Button btSearch = findViewById(R.id.btSearch);
+        btSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Search", Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.actionbar_actions, menu) ;
-        return true ;
-    }
-
-
 }
