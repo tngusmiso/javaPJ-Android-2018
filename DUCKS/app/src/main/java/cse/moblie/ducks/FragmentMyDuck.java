d90c4b380d5edb0c115f63226d91454450ab1267
tngusmiso <k1miso012@gmail.com>
2018-11-17 22:57:37 +0900

    [1117] Change all Activities to Fragments

---
+++ DUCKS/app/src/main/java/cse/moblie/ducks/FragmentMyDuck.java
@@ -0,0 +1,23 @@
+package cse.moblie.ducks;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMyDuck extends Fragment {
+
+
+    public FragmentMyDuck() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.my_duck_fragment, container, false);
+    }
+
+}
--- DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
+++ DUCKS/app/src/main/java/cse/moblie/ducks/MainActivity.java
@@ -1,6 +1,8 @@
 package cse.moblie.ducks;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,7 +13,14 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private final int FRAG_HOME = 1;
+    private final int FRAG_DUCK = 2;
+    private final int FRAG_MY = 3;
+    private final int FRAG_SCHD = 4;
+    private final int FRAG_SHAR = 5;
+
+    private Button bt_tab1, bt_tab2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,43 +57,87 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        // 마이덕
+        // 위젯에 대한 참조
+        Button btHome = findViewById(R.id.btHome);
         Button btMyDuck = findViewById(R.id.btMyDuck);
-        btMyDuck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MyDuck.class);
-                startActivity(intent);
-            }
-        });
-        // 마이페이지
         Button btMyPage = findViewById(R.id.btMypage);
-        btMyPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MyPage.class);
-                startActivity(intent);
-            }
-        });
-        // 스케줄
         Button btSchedule = findViewById(R.id.btSchedule);
-        btSchedule.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Schedule.class);
-                startActivity(intent);
-            }
-        });
-        // 양도 나눔
-        Button btNanum = findViewById(R.id.btNanum);
-        btNanum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Sharing.class);
-                startActivity(intent);
-            }
-        });
+        Button btSharing = findViewById(R.id.btSharing);
+
+        // 탭 버튼에 대한 리스너 연결
+        btHome.setOnClickListener(this);
+        btMyDuck.setOnClickListener(this);
+        btMyPage.setOnClickListener(this);
+        btSchedule.setOnClickListener(this);
+        btSharing.setOnClickListener(this);
+
+        // 임의로 액티비티 호출 시점에 어느 프레그먼트를 프레임레이아웃에 띄울 것인지를 정함
+        callFragment(FRAG_MY);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btHome :
+                Toast.makeText(getApplicationContext(),"HOME",Toast.LENGTH_LONG).show();
+                callFragment(FRAG_HOME);
+                break;
+
+            case R.id.btMyDuck :
+                callFragment(FRAG_DUCK);
+                break;
+
+            case R.id.btMypage :
+                callFragment(FRAG_MY);
+                break;
+
+            case R.id.btSchedule :
+                callFragment(FRAG_SCHD);
+                break;
+
+            case R.id.btSharing :
+                callFragment(FRAG_SHAR);
+                break;
+        }
+    }
+
+    private void callFragment(int frament_no){
+
+        // 프래그먼트 사용을 위해
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (frament_no){
+            case 1:
+                FragmentHome f1 = new FragmentHome();
+                transaction.replace(R.id.flContainer, f1);
+                transaction.commit();
+                break;
+
+            case 2:
+                FragmentMyDuck f2 = new FragmentMyDuck();
+                transaction.replace(R.id.flContainer, f2);
+                transaction.commit();
+                break;
+
+            case 3:
+                FragmentMyPage f3 = new FragmentMyPage();
+                transaction.replace(R.id.flContainer, f3);
+                transaction.commit();
+                break;
+
+            case 4:
+                FragmentSchedule f4 = new FragmentSchedule();
+                transaction.replace(R.id.flContainer, f4);
+                transaction.commit();
+                break;
+
+            case 5:
+                FragmentSharing f5 = new FragmentSharing();
+                transaction.replace(R.id.flContainer, f5);
+                transaction.commit();
+                break;
+        }
 
     }
+
 }
