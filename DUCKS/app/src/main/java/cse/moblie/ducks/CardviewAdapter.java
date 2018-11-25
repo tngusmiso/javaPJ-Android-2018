8286e32b9e67b4f02f08ba326a23caf61b4640e6
tngusmiso <k1miso012@gmail.com>
2018-11-25 23:58:04 +0900

    [1125] Cardview Adapter

---
+++ DUCKS/app/src/main/java/cse/moblie/ducks/CardviewAdapter.java
@@ -0,0 +1,79 @@
+package cse.moblie.ducks;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import cse.moblie.ducks.fragment.FragmentSchedule;
+
+/**
+ * Created by js on 2017-03-18.
+ */
+
+public class CardviewAdapter extends RecyclerView.Adapter<CardviewAdapter.ViewHolder> {
+
+    private ArrayList<FragmentSchedule.item> mDataset; //FragmentSchedule에 item class를 정의해 놓았음
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // 사용될 항목들 선언
+        public TextView tvMonth;
+        public TextView tvDate;
+        public TextView tvTime;
+        public TextView tvLocation;
+        public TextView tvTitle;
+
+        public ViewHolder(View v) {
+            super(v);
+
+            tvMonth = (TextView)v.findViewById(R.id.sch_month);
+            tvDate = (TextView) v.findViewById(R.id.sch_date);
+            tvTime = (TextView) v.findViewById(R.id.sch_time);
+            tvLocation = (TextView) v.findViewById(R.id.sch_location);
+            tvTitle = (TextView) v.findViewById(R.id.sch_title);
+        }
+    }
+
+    // 생성자 - 넘어 오는 데이터파입에 유의해야 한다.
+    public CardviewAdapter(ArrayList<FragmentSchedule.item> myDataset) {
+        mDataset = myDataset;
+    }
+
+
+    //뷰홀더
+    // Create new views (invoked by the layout manager)
+    @Override
+    public CardviewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.schedule_cardview, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.tvMonth.setText(mDataset.get(position).getMonth());
+        holder.tvDate.setText(mDataset.get(position).getDate());
+        holder.tvTime.setText(mDataset.get(position).getTime());
+        holder.tvLocation.setText(mDataset.get(position).getLocation());
+        holder.tvTitle.setText(mDataset.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
--- DUCKS/app/src/main/java/cse/moblie/ducks/fragment/FragmentSchedule.java
+++ DUCKS/app/src/main/java/cse/moblie/ducks/fragment/FragmentSchedule.java
@@ -2,6 +2,8 @@ package cse.moblie.ducks.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +33,7 @@ public class FragmentSchedule extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.schedule_fragment, container, false);
-        materialCalendarView = (MaterialCalendarView)v.findViewById(R.id.calendarView);
+        materialCalendarView = (MaterialCalendarView) v.findViewById(R.id.calendarView);
 
         materialCalendarView.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
@@ -45,7 +47,45 @@ public class FragmentSchedule extends Fragment {
                 new SaturdayDecorator(),
                 new TodayDecorator());
 
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.schedule_fragment, container, false);
     }
+
+    public class item {
+        String month;
+        String date;
+        String time;
+        String location;
+        String title;
+
+        public item(String month, String date, String time, String location, String title) {
+            this.month = month;
+            this.date = date;
+            this.time = time;
+            this.location = location;
+            this.title = title;
+        }
+
+        public String getMonth() {
+            return month;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+
 }
