a9ac5ce3d5b1b03c7e0a2e8cafd5c84afcd9eaf2
tngusmiso <k1miso012@gmail.com>
2018-12-19 22:05:09 +0900

    [1219] Show schedule Cardview - RecyclerView

--- DUCKS/app/src/main/java/cse/moblie/ducks/fragment/FragmentSchedule.java
+++ DUCKS/app/src/main/java/cse/moblie/ducks/fragment/FragmentSchedule.java
@@ -20,11 +20,16 @@ import cse.moblie.ducks.decorator.SaturdayDecorator;
 import cse.moblie.ducks.decorator.SundayDecorator;
 import cse.moblie.ducks.decorator.TodayDecorator;
 import cse.moblie.ducks.recycler.CardAdapter;
+import cse.moblie.ducks.recycler.ScheduleItem;
 
 public class FragmentSchedule extends Fragment {
 
-
     private static MaterialCalendarView materialCalendarView;
+
+    private RecyclerView mRecycler_schedule;
+    private RecyclerView.LayoutManager mLayoutManager_schedule;
+    ArrayList<ScheduleItem> arrayList_schedule = new ArrayList<>();
+    private CardAdapter scheduleAdapter;
 
     public FragmentSchedule() {
         // Required empty public constructor
@@ -34,8 +39,8 @@ public class FragmentSchedule extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.schedule_fragment, container, false);
-        materialCalendarView = (MaterialCalendarView) v.findViewById(R.id.calendarView);
+        View view = inflater.inflate(R.layout.schedule_fragment, container, false);
+        materialCalendarView = (MaterialCalendarView) view.findViewById(R.id.calendarView);
 
         materialCalendarView.setSelectedDate(CalendarDay.today());
         materialCalendarView.state().edit()
@@ -52,8 +57,23 @@ public class FragmentSchedule extends Fragment {
 
 
 
-    // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.schedule_fragment, container, false);
+        arrayList_schedule.add(new ScheduleItem(0,"DEC", "25","18:00","20:30","크리스마스 파티","서울시 어쩌구 저쩌구",null,0));
+        arrayList_schedule.add(new ScheduleItem(0,"DEC", "31","22:00","23:59","새해 파티","서울시 어쩌구 저쩌구",null,0));
+
+        mRecycler_schedule = view.findViewById(R.id.rvSchedule);
+        mRecycler_schedule.setHasFixedSize(true);
+
+        mLayoutManager_schedule = new LinearLayoutManager(getContext());
+        //((LinearLayoutManager) mLayoutManager_schedule).setOrientation(LinearLayout.HORIZONTAL);
+        mRecycler_schedule.setLayoutManager(mLayoutManager_schedule);
+
+        scheduleAdapter = new CardAdapter(arrayList_schedule);
+
+
+        mRecycler_schedule.setAdapter(scheduleAdapter);
+
+        scheduleAdapter.notifyDataSetChanged();
+        return view;
     }
 
 
